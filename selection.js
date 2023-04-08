7a83fdcacb684532b77e8949ace25216c96273b9
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-04-08 13:14:47 +0200

    qucksort inutile ?

---
+++ selection.js
@@ -0,0 +1,19 @@
+// function selectionSort(array) {
+//   for (let i = 0; i < array.length - 1; i++) {
+//     let minIndex = i;
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] < array[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+//     [array[i], array[minIndex]] = [array[minIndex], array[i]];
+//   }
+//   console.log(array);
+// }
+
+
+function selectionSort(array) {
+  console.log(array.sort((a, b) => b - a));
+}
+
+selectionSort([451, 2, 65, 4, 7, 98, 2, 3, 1]);
